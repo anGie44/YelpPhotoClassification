a7635a813876578bb3e2884daeda9ac24008e61d
anGie44 <angelinepinilla@gmail.com>
2016-02-29 13:10:09 -0500

    Update imgData.py

--- imgData.py
+++ imgData.py
@@ -8,14 +8,14 @@ class YelpImageDataset(object):
 			img = cv2.imread(i, 0)
 			self.train_imgs.append(img)
 
-	def _define_surf_features:
+	def define_surf_features:
 		surf = cv2.xfeatures2d.SURF_create(400)
 		surf.setExtended(True)
 		for i in range(len(self.train_imgs)):
 			(kp, des) = surf.detectAndCompute(self.train_imgs[i], None)
 			self.surf_features.append((kp,des))
 
-	def _define_sift_features:
+	def define_sift_features:
 		sift = cv2.xfeatures2d.SIFT_create()
 		for i in range(len(self.train_imgs)):
 			kp = sift.detectAndCompute(self.train_imgs[i], None)
